a1d3d60a40cfc199196ea14f27312b05450c0ce4
Tadeusz Czyżycki <tadeusz.czyzycki98@gmail.com>
2023-08-06 18:31:11 +0200

    Zmiana pierwszych liter na duże, Przeiterowanie słownika

--- lerning-git-task.py
+++ lerning-git-task.py
@@ -3,3 +3,12 @@ lista_zakupów = {
     "warzywniak": ["marcher", "seler", "rukola"]
 }
 
+
+lista_zakupów_z_dużej_litery = {key.title(): [item.title(
+) for item in value] for key, value in lista_zakupów.items()}
+
+
+for shop, items in lista_zakupów_z_dużej_litery.items():
+    sentence = f"Idę do {shop} i kupuję tam {items}."
+    print(sentence)
+
