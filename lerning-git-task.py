dc4bb611d9c2280623b686543fb2277806fc9093
Tadeusz Czyżycki <tadeusz.czyzycki98@gmail.com>
2023-08-06 18:38:01 +0200

    Policzenie produktów

--- lerning-git-task.py
+++ lerning-git-task.py
@@ -12,3 +12,10 @@ for shop, items in lista_zakupów_z_dużej_litery.items():
     sentence = f"Idę do {shop} i kupuję tam {items}."
     print(sentence)
 
+
+liczba_produktów = 0
+for items in lista_zakupów.values():
+    liczba_produktów += len(items)
+
+
+print(f"W sumie kupuję {liczba_produktów} produktów.")
